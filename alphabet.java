060029af65b020154a6a9ad5b95bda7a961cc3d3
Abinaya97 <abimasilamani100697@gmail.com>
2017-06-16 12:20:38 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -9,5 +9,7 @@ Scanner sc=new Scanner(System.in);
 ch=sc.next().charAt(0);
 if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
 System.out.println("alphabet");
+  else
+    System.out.println(" not a alphabet");
 }
 }
