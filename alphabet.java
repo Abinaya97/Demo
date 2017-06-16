885847c148249e9b22fcea2140fc9077155a1933
Abinaya97 <abimasilamani100697@gmail.com>
2017-06-16 12:03:29 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class alphabet
+{
+public static void main(String args[])
+{
+char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
+System.out.println("alphabet");
+}
+}
